54ee7b164a6a0d6ddae77a0b2d7eac644173bf62
Logan Page <pagelr@dukes.jmu.edu>
2023-11-01 19:14:54 -0400

    sign hopefully easier to display, compatible with whole numbers

--- src/utilities/Fractions.java
+++ src/utilities/Fractions.java
@@ -2,29 +2,65 @@ package utilities;
 
 public class Fractions 
 {
+	private Boolean isNegative;
 	private Integer wholeNumber;
 	private Integer numerator;
 	private Integer denominator;
 	
-	public Fractions(Integer numerator, Integer denominator)
+	//Turns a whole number into an improper fraction.
+	public Fractions(Integer whole)
 	{
-		this(0, numerator, denominator);
+		this(false, 0, whole * whole, whole);
 	}
 	
+	//Turns a whole number into a negative improper fraction.
+	public Fractions(Boolean negative, Integer whole) 
+	{
+		this(negative, 0, whole * whole, whole);
+	}
+	
+	//Creates a fraction with no whole number
+	public Fractions(Integer numerator, Integer denominator)
+	{
+		this(false, 0, numerator, denominator);
+	}
+	
+	//Creates a fraction with a whole number
 	public Fractions(Integer wholeNumber, Integer numerator, Integer denominator)
 	{
-		if(numerator == null || denominator == null)
+		this(false, wholeNumber, numerator, denominator);
+	}
+	
+	//Creates a negative fraction with no whole number
+	public Fractions(Boolean negative, Integer numerator, Integer denominator)
+	{
+		this(negative, 0, numerator, denominator);
+	}
+	
+	//Creates a negative fraction with a whole number
+	public Fractions(Boolean negative, Integer wholeNumber, Integer numerator, Integer denominator)
+	{
+		if(negative == null || numerator == null || denominator == null)
 		{
 			throw new IllegalArgumentException();
-		} else if(numerator < 0 && denominator < 0)
+		} else if (denominator == 0)
+		{
+			throw new IllegalArgumentException();
+		} else if (numerator < 0 && denominator < 0)
 		{
 			numerator *= -1;
 			denominator *= -1;
 		}
 		
+		this.isNegative = negative;
 		this.wholeNumber = wholeNumber;
 		this.numerator = numerator;
 		this.denominator = denominator;
+	}
+	
+	public Boolean getIsNegative()
+	{
+		return this.isNegative;
 	}
 	
 	public Integer getNumerator()
@@ -42,27 +78,24 @@ public class Fractions
 		return this.wholeNumber;
 	}
 	
-	public static Integer changeSign(Integer number) 
+	public static void changeSign(Fractions fraction) 
 	{
-		if(number == null)
+		if (fraction == null)
 		{
 			throw new IllegalArgumentException();
 		}
-		return number *= -1;
+		
+		fraction.setSign();
 	}
 	
-	public void setDenominator(Integer number)
+	public void setSign()
 	{
-		this.denominator = number;
-	}
-	
-	public void setNumerator(Integer number)
-	{
-		this.numerator = number;
-	}
-	
-	public void setWholeNumber(Integer number)
-	{
-		this.wholeNumber = number;
+		if(this.isNegative == false)
+		{
+			this.isNegative = true;
+		} else
+		{
+			this.isNegative = false;
+		}
 	}
 }
