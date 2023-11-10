6ed94d4f94af1d765f55d5ed948a97f56e2e440a
Logan Page <pagelr@dukes.jmu.edu>
2023-11-10 15:37:16 -0500

    Created mediant, power, inverse methods + testing

--- src/utilities/Calculations.java
+++ src/utilities/Calculations.java
@@ -2,6 +2,12 @@ package utilities;
 
 public class Calculations 
 {
+	/**
+	 * Adds two fractions together.
+	 * @param number1 First Fraction.
+	 * @param number2 Second Fraction.
+	 * @return Fractions the addition of the two fractions.
+	 */
 	public static Fractions addition(Fractions number1, Fractions number2)
 	{
 		if(number1 == null || number2 == null)
@@ -32,6 +38,12 @@ public class Calculations
 		return result;
 	}
 	
+	/**
+	 * Subtracts two fractions.
+	 * @param number1 The fraction being subtracted from.
+	 * @param number2 The fraction that is being taken.
+	 * @return Fractions the sbutraction of the two fractions.
+	 */
 	public static Fractions subtraction(Fractions number1, Fractions number2)
 	{
 		if(number1 == null || number2 == null)
@@ -62,6 +74,12 @@ public class Calculations
 		return result;
 	}
 	
+	/**
+	 * Multiplies two fractions together.
+	 * @param number1 The first fraction.
+	 * @param number2 The second fraction.
+	 * @return Fractions the product of the two fractions.
+	 */
 	public static Fractions multiplication(Fractions number1, Fractions number2)
 	{
 		if(number1 == null || number2 == null)
@@ -89,6 +107,12 @@ public class Calculations
 		return result;
 	}
 	
+	/**
+	 * Divides two fractions together.
+ 	 * @param number1 The fraction being divided.
+	 * @param number2 The fraction that is the amount of dividing.
+	 * @return Fractions the quotient of the two fractions.
+	 */
 	public static Fractions division(Fractions number1, Fractions number2)
 	{
 		Fractions reciprocal;
@@ -101,17 +125,107 @@ public class Calculations
 		Fractions one = Calculations.improper(number1);
 		Fractions two = Calculations.improper(number2);
 		
-		if (two.getNumerator() < 0)
-		{
-			reciprocal = new Fractions(two.getDenominator() * -1, two.getNumerator() * -1);
-		} else
-		{
-			reciprocal = new Fractions(two.getDenominator(), two.getNumerator());
-		}
+		reciprocal = Calculations.inverse(two);
 		
 		return Calculations.multiplication(one, reciprocal);
 	}
 	
+	/**
+	 * Turns a fraction into its multiplicative inverse.
+	 * @param mixedNumber The fraction to be inverted.
+	 * @return Fractions that inverted fraction.
+	 */
+	public static Fractions inverse(Fractions mixedNumber) 
+	{
+		if (mixedNumber == null)
+		{
+			throw new IllegalArgumentException();
+		}
+		
+		Fractions improper = Calculations.improper(mixedNumber);
+		Integer numerator = improper.getNumerator();
+		Integer denominator = improper.getDenominator();
+		
+		if(numerator < 0)
+		{
+			return new Fractions(true, denominator, numerator * -1);
+		} else
+		{
+			return new Fractions(false, denominator, numerator);
+		}
+	}
+	
+	/**
+	 * Finds the mediant of two fractions.
+	 * @param number1 The first fraction.
+	 * @param number2 The second fraction.
+	 * @return Fractions the medaint of the two fractions.
+	 */
+	public static Fractions mediant(Fractions number1, Fractions number2)
+	{
+		if(number1 == null || number2 == null)
+		{
+			throw new IllegalArgumentException();
+		}
+			
+		Fractions one = Calculations.improper(number1);
+		Fractions two = Calculations.improper(number2);		
+		Integer numerator = one.getNumerator() + two.getNumerator();
+		Integer denominator = one.getDenominator() + two.getDenominator();
+		Boolean isNegative = false;
+			
+		if (numerator < 0)
+		{
+			isNegative = true;
+			numerator *= -1;
+		}
+			
+		Fractions result = new Fractions(isNegative, numerator, denominator);
+			
+		return result;
+	}
+	
+	/**
+	 * Find the fraction to a certain power.
+	 * @param fraction The fraction being applied the exponent.
+	 * @param power The power of the exponent.
+	 * @return Fractions the power multiplied fraction.
+	 */
+	public static Fractions power(Fractions fraction, Integer power)
+	{
+		if (fraction == null || power == null)
+		{
+			throw new IllegalArgumentException();
+		}
+		
+		Fractions improper = Calculations.improper(fraction);
+		Integer origin_N = improper.getNumerator();
+		Integer origin_D = improper.getDenominator();
+		
+		Integer new_N = 1;
+		Integer new_D = 1;
+		Boolean isNegative = false;
+		
+		for (int i = 0; i < power; i++)
+		{
+			new_N *= origin_N;
+			new_D *= origin_D;
+		}
+		
+		if(new_N < 0)
+		{
+			isNegative = true;
+			new_N *= -1;
+		}
+		
+		return new Fractions(isNegative, new_N, new_D);
+	}
+
+	/**
+	 * Turns a mixed fraction into an improper fraction.
+	 * @param mixedNumber the fraction to be converted.
+	 * @return Fractions the improper form of the fraction.
+	 */
 	public static Fractions improper(Fractions mixedNumber)
 	{
 		Integer whole = mixedNumber.getWholeNumber();
@@ -128,6 +242,11 @@ public class Calculations
 		return new Fractions(numerator, bottom);
 	}
 	
+	/**
+	 * Reduces the fraction to simplest form.
+	 * @param irreduced The fraction to be reduced.
+	 * @return Fractions the simplified form of the fraction.
+	 */
 	public static Fractions reduce(Fractions irreduced)
 	{		
 		int newNum1 = irreduced.getNumerator();
@@ -155,6 +274,11 @@ public class Calculations
 	    return new Fractions(isNegative, numerator, denominator);
 	}
 	
+	/**
+	 * Places a fraction in proper form.
+	 * @param improper The fraction to be converted.
+	 * @return Fractions the proper form of the fraction.
+	 */
 	public static Fractions proper(Fractions improper)
 	{		
 		Integer numerator = improper.getNumerator();
