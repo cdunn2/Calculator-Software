e216f8368ef521a825c5554ef7fa4147abf0b052
Colin McCaffery <mccaf2cm@dukes.jmu.edu>
2023-11-17 00:31:45 -0500

    Got rid of 2 unused imports

--- src/gui/FragileWindow.java
+++ src/gui/FragileWindow.java
@@ -6,8 +6,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
 import java.util.Locale;
 import java.util.ResourceBundle;
 import gui.display.Display;
