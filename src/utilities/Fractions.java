7b36452402690bba801d3a211302cbcea13513cc
Logan Page <pagelr@dukes.jmu.edu>
2023-12-10 01:55:43 -0500

    added documentation to fractions class

--- src/utilities/Fractions.java
+++ src/utilities/Fractions.java
@@ -7,37 +7,64 @@ public class Fractions
 	private Integer numerator;
 	private Integer denominator;
 	
-	//Turns a whole number into an improper fraction.
+	/**
+	 * Turns a whole number into an improper fraction.
+	 * @param whole The whole number.
+	 */
 	public Fractions(Integer whole)
 	{
 		this(false, 0, whole * whole, whole);
 	}
 	
-	//Turns a whole number into a negative improper fraction.
+	/**
+	 * Turns a whole number into a negative improper fraction.
+	 * @param negative determines sign of fraction.
+	 * @param whole The whole number.
+	 */
 	public Fractions(Boolean negative, Integer whole) 
 	{
 		this(negative, 0, whole * whole, whole);
 	}
 	
-	//Creates a fraction with no whole number
+	/**
+	 * Creates a fraction with no whole number.
+	 * @param numerator The top number of a fraction.
+	 * @param denominator The bottom number of a fraction.
+	 */
 	public Fractions(Integer numerator, Integer denominator)
 	{
 		this(false, 0, numerator, denominator);
 	}
 	
-	//Creates a fraction with a whole number
+	/**
+	 * Creates a fraction with a whole number.
+	 * @param wholeNumber The whole number.
+	 * @param numerator The top number of the fraction.
+	 * @param denominator The bottom number of the fraction.
+	 */
 	public Fractions(Integer wholeNumber, Integer numerator, Integer denominator)
 	{
 		this(false, wholeNumber, numerator, denominator);
 	}
 	
-	//Creates a negative fraction with no whole number
+	/**
+	 * Creates a negative fraction with no whole number
+	 * @param negative Determines the sign of the fraction.
+	 * @param numerator The top number of a fraction.
+	 * @param denominator The bottom number of a fraction.
+	 */
 	public Fractions(Boolean negative, Integer numerator, Integer denominator)
 	{
 		this(negative, 0, numerator, denominator);
 	}
 	
-	//Creates a negative fraction with a whole number
+	/**
+	 * Creates a negative fraction with a whole number.
+	 * @param negative Determines the sign of the fraction.
+	 * @param wholeNumber The whole number of a fraction.
+	 * @param numerator The top number of a fraction.
+	 * @param denominator The bottom number of a fraction.
+	 */
 	public Fractions(Boolean negative, Integer wholeNumber, Integer numerator, Integer denominator)
 	{
 		if(negative == null || numerator == null || denominator == null)
@@ -58,26 +85,46 @@ public class Fractions
 		this.denominator = denominator;
 	}
 	
+	/**
+	 * Gets the boolean that determines if a fraction is negative.
+	 * @return
+	 */
 	public Boolean getIsNegative()
 	{
 		return this.isNegative;
 	}
 	
+	/**
+	 * Gets the numerator of a fraction.
+	 * @return
+	 */
 	public Integer getNumerator()
 	{
 		return this.numerator;
 	}
 	
+	/**
+	 * Gets the denominator of a fraction.
+	 * @return
+	 */
 	public Integer getDenominator()
 	{
 		return this.denominator;
 	}
 	
+	/**
+	 * Gets the whole number.
+	 * @return
+	 */
 	public Integer getWholeNumber()
 	{
 		return this.wholeNumber;
 	}
 	
+	/**
+	 * Changes the boolean of the sign to the opposite boolean.
+	 * @param fraction The fraction to have its sign changed.
+	 */
 	public static void changeSign(Fractions fraction) 
 	{
 		if (fraction == null)
@@ -88,6 +135,9 @@ public class Fractions
 		fraction.setSign();
 	}
 	
+	/**
+	 * Sets the sign.
+	 */
 	public void setSign()
 	{
 		if(this.isNegative == false)
@@ -98,7 +148,12 @@ public class Fractions
 			this.isNegative = false;
 		}
 	}
-	//Helper method to convert a string to a fraction.
+	
+	/**
+	 * Helper method to convert a string to a fraction.
+	 * @param s The fraction in string form from the display.
+	 * @return Fractions fraction object.
+	 */
 	public static Fractions parseFractions(String s) {
 	  boolean isNegative = false;
 	  int numerator = -1;
@@ -128,13 +183,16 @@ public class Fractions
 	    numerator = 0;
 	  }
 	  try {
-    denominator = Integer.parseInt(s.substring(s.indexOf("/") + 1));
+		  denominator = Integer.parseInt(s.substring(s.indexOf("/") + 1));
 	  } catch(NumberFormatException e) {
 	    denominator = 1;
 	  }
-    return new Fractions(isNegative, whole, numerator, denominator);
+	  return new Fractions(isNegative, whole, numerator, denominator);
 	}
 	
+	/**
+	 * Converts a fraction object into a string.
+	 */
 	public String toString() {
 	  String str = "";
 	  if(this.isNegative)
@@ -143,6 +201,11 @@ public class Fractions
 	  return str;
 	}
 	
+	/**
+	 * Compares a fraction object to another fraction object.
+	 * @param other The other fraction.
+	 * @return boolean true if two fractions are equal and false otherwise.
+	 */
 	public boolean equals(Fractions other) {
 	  if(this.isNegative == other.getIsNegative() && this.wholeNumber == other.getWholeNumber() && 
 	      this.numerator == other.getNumerator() && this.denominator == other.getDenominator()){
